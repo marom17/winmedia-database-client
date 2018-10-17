046ea58d99ef1511989563b64ac65ceb649acd43
marom17 <romain.flac@gmail.com>
2018-10-17 17:34:17 +0200

    adding test media info

--- Winmedia Database Client/App.xaml.cs
+++ Winmedia Database Client/App.xaml.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MediaInfo;
 
 namespace Winmedia_Database_Client
 {
@@ -48,6 +49,16 @@ namespace Winmedia_Database_Client
                 String json = JsonConvert.SerializeObject(config);
                 File.WriteAllText(filepath, json);
             }
+
+            /// Get Media Info
+            MediaInfo.MediaInfo MI = new MediaInfo.MediaInfo();
+            MI.Open("./test.flac");
+            int duration = Convert.ToInt32(MI.Get(StreamKind.Audio, 0, "Duration"))/1000;
+            int fileLength = Convert.ToInt32(MI.Get(0, 0, "FileSize"));
+            int second = duration % 60;
+            int minute = duration / 60;
+            Debug.WriteLine("File length: " + fileLength);
+            Debug.WriteLine("Duration: " + minute + " min " + second + " s");
             
         }
     }
