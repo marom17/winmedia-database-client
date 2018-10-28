7160daf13a5a7451512212b440a3de94dd53bda4
Marom <romain.flac@gmail.com>
2018-10-28 14:15:54 +0100

    Update the start time when adding a new element in the playlist

--- Winmedia Database Client/helpers/PlaylistElement.cs
+++ Winmedia Database Client/helpers/PlaylistElement.cs
@@ -10,6 +10,19 @@ namespace Winmedia_Database_Client
     public class PlaylistElement
     {
         private Music music;
+        private TimeSpan _start = new TimeSpan(0, 0, 0);
+        public TimeSpan Start { get {
+                return _start;
+            }
+            set
+            {
+                _start = value;
+            }
+        }
+        public TimeSpan PrettyTime { get => music.PrettyTime; }
+        public String Performer { get => music.Artist; }
+        public String Title { get => music.Title; }
+        public Music Music { get => music; }
 
         public PlaylistElement(Music music)
         {
--- Winmedia Database Client/pages/PlaylistShow.xaml.cs
+++ Winmedia Database Client/pages/PlaylistShow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,14 @@ namespace Winmedia_Database_Client
     /// </summary>
     public partial class PlaylistShow : Page
     {
+        private Boolean isDraging = false;
+        private Boolean dropOnElement = false;
+        private Boolean isUpdating = false;
+
         public PlaylistShow()
         {
             InitializeComponent();
+            ((INotifyCollectionChanged)this.PlList.Items).CollectionChanged += PlList_CollectionChanged;
         }
 
         public void ShowPlaylist(List<PlaylistElement> playlist)
@@ -31,20 +38,136 @@ namespace Winmedia_Database_Client
 
             foreach (var item in playlist)
             {
-                ListBoxItem boxItem = new ListBoxItem();
-                boxItem.Content = item;
-                this.PlList.Items.Add(item);
+                ListViewItem viewItem = new ListViewItem();
+                viewItem.Content = item;
+                this.PlList.Items.Add(viewItem);
             }
         }
 
         private void PlList_Drop(object sender, DragEventArgs e)
         {
             Music music = (Music)e.Data.GetData(typeof(Music));
+            PlaylistElement element = (PlaylistElement)e.Data.GetData(typeof(PlaylistElement));
+            this.ListChange(music, element, sender);
+
+
+        }
+
+        private void ListViewItem_Drop(object sender, DragEventArgs e)
+        {
+            dropOnElement = true;
+            Music music = (Music)e.Data.GetData(typeof(Music));
+            PlaylistElement element = (PlaylistElement)e.Data.GetData(typeof(PlaylistElement));
+
+            this.ListChange(music, element, sender);
+        }
+
+        private void ListChange(Music music, PlaylistElement element, object sender)
+        {
             if (music != null)
             {
-                ListBoxItem boxItem = new ListBoxItem();
-                boxItem.Content = new PlaylistElement(music);
-                this.PlList.Items.Add(boxItem);
+                if (!dropOnElement || sender.GetType() == typeof(ListViewItem))
+                {
+                    if(sender.GetType() == typeof(ListViewItem))
+                    {
+                        int index = this.PlList.Items.IndexOf(((ListViewItem)sender));
+                        ListViewItem viewItem = new ListViewItem();
+                        viewItem.Content = new PlaylistElement(music);
+                        this.PlList.Items.Insert(index, viewItem);
+                    }
+                    else
+                    {
+                        ListViewItem viewItem = new ListViewItem();
+                        viewItem.Content = new PlaylistElement(music);
+                        this.PlList.Items.Add(viewItem);
+                    }                    
+                }
+                else
+                {
+                    dropOnElement = false;
+                }
+
+            }
+            if (element != null)
+            {
+
+            }
+        }
+
+        private void PlList_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (!isUpdating)
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    this.calculateTime();
+                }
+                if (e.Action == NotifyCollectionChangedAction.Move)
+                {
+                    this.calculateTime();
+                }
+                if (e.Action == NotifyCollectionChangedAction.Remove)
+                {
+                    this.calculateTime();
+                }
+            }
+        }
+
+        private void calculateTime()
+        {
+            TimeSpan nextStart = new TimeSpan(0, 0, 0);
+            ListView tmp = new ListView();
+            isUpdating = true;
+            foreach(ListViewItem item in this.PlList.Items)
+            {
+                ListViewItem viewItem = new ListViewItem();
+                viewItem.Content = item.Content;
+                tmp.Items.Add(viewItem);
+            }
+            this.PlList.Items.Clear();
+
+            foreach (ListViewItem item in tmp.Items)
+            {
+                ((PlaylistElement)item.Content).Start = nextStart;
+
+                nextStart = new TimeSpan(0, 0, (int)nextStart.TotalSeconds + (int)((PlaylistElement)item.Content).PrettyTime.TotalSeconds);
+
+                ListViewItem viewItem = new ListViewItem();
+                viewItem.Content = item.Content;
+                this.PlList.Items.Add(viewItem);
+                
+            }
+            tmp.Items.Clear();
+            isUpdating = false;
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDraging = false;
+            var item = ((PlaylistElement)(sender as ListViewItem).Content);
+            if (item != null)
+            {
+                PlayerWindow.LoadFile(item.Music);
+
+            }
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = ((PlaylistElement)(sender as ListViewItem).Content);
+            if (item != null)
+            {
+                isDraging = true;
+                DragDropHelper.DragSource = ((ListViewItem)sender);
+            }
+        }
+
+        private void ListViewItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (isDraging)
+            {
+                DragDrop.DoDragDrop(DragDropHelper.DragSource, DragDropHelper.DragSource.Content, DragDropEffects.Move);
+                isDraging = false;
             }
         }
     }
